9641360c9bc69ee6ce673953b4001f20e8eec806
zhudd <ex-zhudd001@cmft.com>
2020-09-29 09:47:23 +0800

    [NEW]第一版编译成功

---
+++ app/src/main/java/com/example/ex_zhudd001/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ex_zhudd001.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
